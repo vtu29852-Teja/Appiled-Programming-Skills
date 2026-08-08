9930aa2b8fa99cd1806c495f07769d43a0b85f0f
vtu29852-Teja <vtu29852@veltech.edu.in>
2026-08-08 14:09:26 +0530

    Time: 4 ms (66.2%), Space: 94.6 MB (44.19%) - LeetHub

---
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -0,0 +1,52 @@
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        if (head == null || head.next == null) {
+            return true;
+        }
+
+        // Find the middle of the list
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        // For odd length, skip the middle node
+        if (fast != null) {
+            slow = slow.next;
+        }
+
+        // Reverse the second half
+        ListNode secondHalf = reverse(slow);
+
+        // Compare first half and reversed second half
+        ListNode firstHalf = head;
+
+        while (secondHalf != null) {
+            if (firstHalf.val != secondHalf.val) {
+                return false;
+            }
+
+            firstHalf = firstHalf.next;
+            secondHalf = secondHalf.next;
+        }
+
+        return true;
+    }
+
+    private ListNode reverse(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        return prev;
+    }
+}
